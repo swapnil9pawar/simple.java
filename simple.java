f6790c7ae39a6f6cf6c82b8a3a3bb08784926006
swapnil9pawar <104321535+swapnil9pawar@users.noreply.github.com>
2022-07-08 20:51:30 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,6 +1,5 @@
-class Simple{
-public static void main(String args[]}
-
-System.out.println("welcome swapnil ");
-}
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello swapnil");  
+    }  
+}  
